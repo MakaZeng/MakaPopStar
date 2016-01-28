2b50f985187e2a22f999d06de073a7d983c0044f
makaChina <610893667@qq.com>
2016-01-29 01:12:47 +0800

    commit

---
+++ Classes/Score/CommonScoreManager.cpp
@@ -0,0 +1,9 @@
+//
+//  CommonScoreManager.cpp
+//  MakaPopStar
+//
+//  Created by chaowualex on 16/1/29.
+//
+//
+
+#include "CommonScoreManager.hpp"
---
+++ Classes/Score/CommonScoreManager.hpp
@@ -0,0 +1,14 @@
+//
+//  CommonScoreManager.hpp
+//  MakaPopStar
+//
+//  Created by chaowualex on 16/1/29.
+//
+//
+
+#ifndef CommonScoreManager_hpp
+#define CommonScoreManager_hpp
+
+#include <stdio.h>
+
+#endif /* CommonScoreManager_hpp */
